8c20959b96dc77fafe1543b21aca8753e1a83daa
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-05-06 14:56:17 +0530

    code updated

--- src/Helpers/downloader.js
+++ src/Helpers/downloader.js
@@ -1,39 +1,34 @@
 const { exec } = require("child_process");
-const path = require("path");
 const fs = require("fs");
 
 class Downloader {
 
     constructor() {
-        this.outputDir = path.join(__dirname, "../downloads");
-
+        this.outputDir = "/tmp/downloads";
         if (!fs.existsSync(this.outputDir)) {
             fs.mkdirSync(this.outputDir, { recursive: true });
         }
     }
-
     download(link) {
-
         if (!link) {
             return Promise.reject(new Error("Download link is required"));
         }
-
         const command = `yt-dlp -f best \
             --no-check-certificate \
             --no-cache-dir \
             --user-agent "Mozilla/5.0" \
             --restrict-filenames \
             --print after_move:filepath \
-            -o "${this.outputDir}/%(title)s.%(ext)s" \
+            -o "${this.outputDir}/video_%(id)s.%(ext)s" \
             "${link}"`;
         return new Promise((resolve, reject) => {
-
-            exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+            exec(command, { maxBuffer: 1024 * 1024 * 20 }, (error, stdout, stderr) => {
+                console.log("STDOUT:", stdout);
+                console.log("STDERR:", stderr);
                 if (error) {
                     return reject(new Error(stderr || error.message));
                 }
                 const filePath = stdout.trim().split("\n").pop();
-
                 if (!filePath || filePath === "") {
                     return reject(new Error("No file downloaded"));
                 }
@@ -44,7 +39,6 @@ class Downloader {
                     message: "Download completed",
                     filePath: filePath
                 });
-
             });
         });
     }
