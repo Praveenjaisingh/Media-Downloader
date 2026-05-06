2a1972133cb3e5e8469c15e67b7e2939f443a605
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-05-06 15:16:38 +0530

    code updated

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
+// const app = require('../src/app');
+
+// module.exports = app;
+
 const app = require('../src/app');
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
